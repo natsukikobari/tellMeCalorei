95c7ea857c342de22555bde1e4daaa0059ee9cb0
Takuya <uoastudent182@gmail.com>
2021-02-06 14:28:26 +0900

    グラフ表示うまくいかない

--- chart.js
+++ chart.js
@@ -1,41 +1,64 @@
 const fs = require('fs');
 var express = require('express');
+const mysql = require('mysql');
 var app = express();
 var path = require('path');
 var Chart = require('chart.js');
 var result =[3,6,9];
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'takuya',
+  password: '0000',
+  database: 'caloriedb'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
 
 app.get('/', function(req, res){
+
+
+
     let _resLine = '<h1>Ereignisse: ' + result+'</h1>';
     console.log('show chart:');
     console.log(_resLine);
-
-    _html = "<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js'></script>"+
-    "<canvas id='bar-chart' width='800' height='450'></canvas>"+
-    "<script>"+
-    "var logChart = new Chart(document.getElementById('bar-chart'), {"+
-    "type: 'horizontalBar',"+
-    "data: {"+
-      "labels: ['Ereignis1', 'Ereignis2', 'Ereignis3'],"+
-      "datasets: ["+
+    connection.query(
+      'SELECT * FROM foodcalorie',
+      (error, results) => {
+        let datadb = JSON.stringify(results);
+        _html = "<script src='https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js'></script>"+
+        "<canvas id='bar-chart' width='800' height='450'></canvas>"+
+        "<script>"+
+        "var logChart = new Chart(document.getElementById('bar-chart'), {"+
+        "type: 'horizontalBar',"+
+        "data: {"+
+        // "labels: ['E', 'Ereignis2', 'Ereignis3'],"+
+        "datasets: ["+
         "{"+
-          "label: 'Aufrufe',"+
-          "backgroundColor: ['#3e95cd', '#8e5ea2','#3cba9f'],"+
-          "data: ["+result[0]+","+result[1]+","+result[2]+"]"+
+        "label: 'Aufrufe',"+
+        "backgroundColor: rgba(255,150,0.9),"+
+        "data: 'datadb'," +
         "}"+
-      "]"+
-      "},"+
-    "options: {"+
-    "legend: { display: false },"+
-      "title: {"+
+        "]"+
+        "},"+
+        "options: {"+
+        "legend: { display: false },"+
+        "title: {"+
         "display: true,"+
         "text: 'Ereignisse '"+
-      "}"+
-    "}"+
-    "});"+
-    "</script>";
-
-    res.send(_html);
+        "}"+
+        "}"+
+        "});"+
+        "</script>";
+        
+        res.send(_html);
+      }
+    );
 });
 
 app.listen(3005);
